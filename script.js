3e756154b390bf5bba2881714c9a810617b3632e
AnvitT <99025362+AnvitT@users.noreply.github.com>
2023-08-08 04:20:21 +0530

    testing splash scrren

---
+++ script.js
@@ -0,0 +1,6 @@
+const splash = document.getElementById(".splash");
+document.addEventListener('click',()=>{
+    setTimeout(()=>{
+        document.getElementById("splash").style.display = "none";
+    },4000)
+})
