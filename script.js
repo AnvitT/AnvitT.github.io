a22023ca5b61ed68e6d8736bcec7d47d5ecf1fa1
AnvitT <99025362+AnvitT@users.noreply.github.com>
2023-08-08 04:53:45 +0530

    testing

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
+
 const splash = document.getElementById(".splash");
-document.addEventListener('DOMContentLoaded',()=>{
+document.addEventListener('click',()=>{
     setTimeout(()=>{
         document.getElementById("splash").style.display = "none";
     },4000)
 })
+
+
+function playSound(){
+    document.getElementById("player").play();
+}
+
+
+
+function hide(){
+    document.getElementById("startButton").style.display = "none";
+    document.getElementById('ball').style.animationPlayState = "running","running";
+    var audio = new Audio('FinalIntro.mp3');
+    audio.play();
+}
+
+
